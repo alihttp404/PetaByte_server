21655e0f4f130e994df9364f0b0b3d44d6f1d068
alihttp404 <ali.mammadov2022@gmail.com>
2025-08-03 22:23:56 +0400

    .

--- dbConfig.js
+++ dbConfig.js
@@ -1,10 +1,13 @@
+require('dotenv').config();
 const { Pool } = require('pg');
-const { DBConnLink } = require('./config'); 
 
 const pool = new Pool({
-    connectionString: DBConnLink,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    host: '127.0.0.1', // Use IP instead of localhost
+    port: process.env.VPS_DB_PORT || 5432,
+    database: process.env.VPS_DB_NAME || 'petcity_db',
+    user: process.env.VPS_DB_USER || 'ali',
+    password: process.env.VPS_DB_PASSWORD,
+    ssl: false
 });
+
 module.exports = pool;
